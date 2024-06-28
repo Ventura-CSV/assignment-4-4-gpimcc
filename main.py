cdf27fd9dbcb80ab2ff0627c17600b9df1fd8612
Gabriella <gabriella_pimentel1@my.vcccd.edu>
2024-06-28 00:00:19 -0400

    completed assignment

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 def main():
 
     numbers = []
+    for i in range(5):
+        num = int(input(f"Enter number {i+1}: "))
+        numbers.append(num)
+        minval = numbers[0]
+        maxval = numbers[0]
+    for num in numbers[1:]:
+        if num < minval:
+            minval = num
+        if num > maxval:
+            maxval = num
+
     """
     ########################################
     Code Your Program here
